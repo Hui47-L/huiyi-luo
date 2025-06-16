2741a5eaf49b64e333994b6cde308d85b6f56df3
Hui47-L <luohuiyi47@gmail.com>
2025-06-16 20:06:20 +0900

    9

--- site.config.ts
+++ site.config.ts
@@ -50,17 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  // navigationStyle: 'default'
-  navigationStyle: 'custom',
-  navigationLinks: [
-     {
-       title: 'About',
-       pageId: '214195f3c05b80f9a711fe1c2668342c'
-     },
-     {
-       title: 'Contact',
-       pageId: '214195f3c05b802dab0bcc5c53f36e4e'
-     }
-   ]
+  navigationStyle: 'custom'
+  // navigationStyle: 'custom',
+  // navigationLinks: [
+  //   {
+  //     title: 'About',
+  //     pageId: '214195f3c05b80f9a711fe1c2668342c'
+  //   },
+  //   {
+  //     title: 'Contact',
+  //     pageId: '214195f3c05b802dab0bcc5c53f36e4e'
+  //   }
+  // ]
 })
-
